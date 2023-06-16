68a631c85030ea8d62f4a5508b081b522ccbe1f6
cffnpwr <cffnpwr@gmail.com>
2023-06-16 18:39:50 +0900

    chore: nix shellを導入

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A SWC plugin to resolve import extensions.";
+
+  inputs = {
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
+    flake-utils = { url = "github:numtide/flake-utils"; };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, flake-compat, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            cargo
+            rust-analyzer
+          ];
+          shellHook = ''
+            rustup override set nightly
+            rustup target add wasm32-unknown-unknown
+            rustup target add wasm32-wasi
+          '';
+        };
+      });
+}
